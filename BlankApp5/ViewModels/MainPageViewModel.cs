4060f4e30cd49049018571fc6b9fa661624b9b40
spacekey <fumihiro.fukuda@gmail.com>
2020-06-20 22:13:12 +0800

    Add project files.

---
+++ BlankApp5/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Reactive.Bindings;
+
+namespace BlankApp5.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+            
+            Message = new ReactiveProperty<string>();
+
+            InputValue = new ReactiveProperty<string>();
+            InputValue.Subscribe(x => Message.Value = x == null ? "" : $"InputValue changed({x})");
+        }
+
+        public ReactiveProperty<string> Message { get; set; }
+        public ReactiveProperty<string> InputValue { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# BlankApp5 - Entryの変更で何かする
+
+Prism.FormsのテンプレートにReactivePropertyを追加して、MainPageとMainPageViewModelに手を入れただけです。
